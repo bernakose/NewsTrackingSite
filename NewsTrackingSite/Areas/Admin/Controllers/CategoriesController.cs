7c1c3ce219531f2dd4c1edb74572daa6f0f701c1
Berna Köse <anreb_55@hotmail.com>
2021-07-22 20:27:25 +0300

    22.07.2021

--- NewsTrackingSite/Areas/Admin/Controllers/CategoriesController.cs
+++ NewsTrackingSite/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,127 +1,127 @@
-﻿using NewsTrackingSite.Models;
-using NewsTrackingSite.Repositories;
-using System.Web.Mvc;
+﻿//using NewsTrackingSite.Models;
+//using NewsTrackingSite.Repositories;
+//using System.Web.Mvc;
 
-namespace NewsTrackingSite.Areas.Admin.Controllers
-{
-    public class CategoriesController : Controller
-    {
-        KategoriTipRepository _repository = new KategoriTipRepository();
+//namespace NewsTrackingSite.Areas.Admin.Controllers
+//{
+//    public class CategoriesController : Controller
+//    {
+//        KategoriTipRepository _repository = new KategoriTipRepository();
 
-        #region Kategori Listesi
+//        #region Kategori Listesi
 
-        public ActionResult Index()
-        {
-            var kategoriler = _repository.GetirTumu();
-            if (kategoriler.IsSuccessful)
-                return View(kategoriler.Data);
-            return View();
-        }
+//        public ActionResult Index()
+//        {
+//            var kategoriler = _repository.GetirTumu();
+//            if (kategoriler.IsSuccessful)
+//                return View(kategoriler.Data);
+//            return View();
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Kategori Detay
+//        #region Kategori Detay
 
-        public ActionResult Details(int id)
-        {
-            var kategori = _repository.Getir(id);
-            if (kategori.IsSuccessful)
-                return View(kategori.Data);
-            return RedirectToAction("KategoriListesi");
-        }
+//        public ActionResult Details(int id)
+//        {
+//            var kategori = _repository.Getir(id);
+//            if (kategori.IsSuccessful)
+//                return View(kategori.Data);
+//            return RedirectToAction("KategoriListesi");
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Kategori Ekle
+//        #region Kategori Ekle
 
-        public ActionResult Create()
-        {
-            return View();
-        }
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
 
-        [HttpPost]
-        public ActionResult Create(Category kayit)
-        {
-            if (ModelState.IsValid)
-            {
-                var islemSonuc = _repository.Kaydet(kayit);
-                if (islemSonuc.IsSuccessful)
-                {
-                    return RedirectToAction("KategoriListesi");
-                }
-                else
-                {
-                    ModelState.AddModelError("", islemSonuc.Message);
-                    return View();
-                }
-            }
-            else
-            {
-                return View();
-            }
-        }
+//        [HttpPost]
+//        public ActionResult Create(Category kayit)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var islemSonuc = _repository.Kaydet(kayit);
+//                if (islemSonuc.IsSuccessful)
+//                {
+//                    return RedirectToAction("KategoriListesi");
+//                }
+//                else
+//                {
+//                    ModelState.AddModelError("", islemSonuc.Message);
+//                    return View();
+//                }
+//            }
+//            else
+//            {
+//                return View();
+//            }
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Kategori Düzenle
+//        #region Kategori Düzenle
 
-        public ActionResult Edit(int id)
-        {
-            var kategori = _repository.Getir(id);
-            if (kategori.IsSuccessful)
-                return View(kategori.Data);
-            return RedirectToAction("KategoriListesi");
-        }
+//        public ActionResult Edit(int id)
+//        {
+//            var kategori = _repository.Getir(id);
+//            if (kategori.IsSuccessful)
+//                return View(kategori.Data);
+//            return RedirectToAction("KategoriListesi");
+//        }
 
-        [HttpPost]
-        public ActionResult Edit(Category kayit)
-        {
-            if (ModelState.IsValid)
-            {
-                var islemSonuc = _repository.Guncelle(kayit);
-                if (islemSonuc.IsSuccessful)
-                {
-                    return RedirectToAction("KategoriListesi");
-                }
-                else
-                {
-                    ModelState.AddModelError("", islemSonuc.Message);
-                    return View(kayit);
-                }
-            }
-            else
-            {
-                return View();
-            }
-        }
+//        [HttpPost]
+//        public ActionResult Edit(Category kayit)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var islemSonuc = _repository.Guncelle(kayit);
+//                if (islemSonuc.IsSuccessful)
+//                {
+//                    return RedirectToAction("KategoriListesi");
+//                }
+//                else
+//                {
+//                    ModelState.AddModelError("", islemSonuc.Message);
+//                    return View(kayit);
+//                }
+//            }
+//            else
+//            {
+//                return View();
+//            }
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Kategori Sil
+//        #region Kategori Sil
 
-        public ActionResult Delete(int id)
-        {
-            var kategori = _repository.Getir(id);
-            if (kategori.IsSuccessful)
-                return View(kategori.Data);
-            return RedirectToAction("KategoriListesi");
-        }
+//        public ActionResult Delete(int id)
+//        {
+//            var kategori = _repository.Getir(id);
+//            if (kategori.IsSuccessful)
+//                return View(kategori.Data);
+//            return RedirectToAction("KategoriListesi");
+//        }
 
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection bilgiler)
-        {
-            var islemSonuc = _repository.Sil(id);
-            if (islemSonuc.IsSuccessful)
-            {
-                return RedirectToAction("KategoriListesi");
-            }
-            else
-            {
-                ModelState.AddModelError("", islemSonuc.Message);
-                return View();
-            }
-        }
+//        [HttpPost]
+//        public ActionResult Delete(int id, FormCollection bilgiler)
+//        {
+//            var islemSonuc = _repository.Sil(id);
+//            if (islemSonuc.IsSuccessful)
+//            {
+//                return RedirectToAction("KategoriListesi");
+//            }
+//            else
+//            {
+//                ModelState.AddModelError("", islemSonuc.Message);
+//                return View();
+//            }
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
--- NewsTrackingSite/Global.asax.cs
+++ NewsTrackingSite/Global.asax.cs
@@ -1,8 +1,12 @@
+using NewsTrackingSite.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace NewsTrackingSite
@@ -12,7 +16,25 @@ namespace NewsTrackingSite
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Session == null) return;
+
+            var cultureInfo = (CultureInfo)Session["Culture"];
+            if (cultureInfo == null)
+            {
+                var languageName = Country.tr.ToString();
+                cultureInfo = new CultureInfo(languageName);
+                Session["Culture"] = cultureInfo;
+            }
+
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
         }
     }
 }
